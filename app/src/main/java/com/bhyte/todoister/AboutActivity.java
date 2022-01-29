5963462d3fa6ef3bde33124e23d7a9a3c4a13447
Braimah-Abiola <braimahabiola.official@gmail.com>
2022-01-29 23:03:34 +0000

    First Commit (SetupSQLDatabase)

---
+++ app/src/main/java/com/bhyte/todoister/AboutActivity.java
@@ -0,0 +1,16 @@
+package com.bhyte.todoister;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import bhyte.todoister.R;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+}
